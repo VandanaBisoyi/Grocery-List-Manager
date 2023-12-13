490f5d9b6dc4857210ca0d7f422e39cec25129fd
VandanaBisoyi <149338273+VandanaBisoyi@users.noreply.github.com>
2023-12-13 15:37:06 -0800

    Grocery list manager initiation

---
+++ src/app/app-grocery/app-grocery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-grocery',
+  templateUrl: './app-grocery.component.html',
+  styleUrls: ['../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
+})
+export class AppGroceryComponent {
+  
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AppGroceryComponent } from './app-grocery/app-grocery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppGroceryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppGroceryComponent]
 })
 export class AppModule { }
