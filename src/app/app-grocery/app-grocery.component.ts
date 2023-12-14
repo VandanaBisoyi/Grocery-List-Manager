5b9e08cc657835f5d27e6e7c6d3dad7776ecccaa
VandanaBisoyi <149338273+VandanaBisoyi@users.noreply.github.com>
2023-12-13 23:19:32 -0800

    Completed Grocery app

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-app-grocery',
   templateUrl: './app-grocery.component.html',
-  styleUrls: ['../../../node_modules/bootstrap/dist/css/bootstrap.min.css']
+  styleUrls: []
 })
 export class AppGroceryComponent {
   task: string;
