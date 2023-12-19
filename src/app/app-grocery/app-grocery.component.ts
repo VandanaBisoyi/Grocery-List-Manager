4a5d9bd829f829051da4e707e73170ec05b0feca
VandanaBisoyi <149338273+VandanaBisoyi@users.noreply.github.com>
2023-12-19 12:25:26 -0800

    added some styling

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-app-grocery',
   templateUrl: './app-grocery.component.html',
-  styleUrls: []
+  styleUrls: ['./app-grocery.component.css']
 })
 export class AppGroceryComponent {
   task: string;
@@ -12,6 +12,15 @@ export class AppGroceryComponent {
   onClick(){
   	this.tasks.push({name: this.task});
   	this.task = '';
+  }
+
+  deleteItem(task: string): void {
+    const index = this.tasks.findIndex(
+      (task: { name: any }) => task.name === task.name
+    );
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+    }
   }
   
 }
